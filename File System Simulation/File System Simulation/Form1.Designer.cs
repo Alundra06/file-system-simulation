f934ac8a108f79d5fc5a2f97674b2ed73a8a529f
Ouassim.Sadellah@gmail.com <Ouassim.Sadellah@gmail.com@c12e58c0-60ec-5d06-71b9-198001d67075>
2012-10-30 20:48:26 +0000

    Hi Team, This is version 1 of the project.

---
+++ File System Simulation/File System Simulation/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace File_System_Simulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.FileView = new System.Windows.Forms.TreeView();
+            this.CreateFiles = new System.Windows.Forms.Button();
+            this.Filereading = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BinaryContent = new System.Windows.Forms.RichTextBox();
+            this.DeleteFiles = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CurrentFolder = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Miramonte", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(403, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File System Simulation";
+            // 
+            // FileView
+            // 
+            this.FileView.Location = new System.Drawing.Point(153, 87);
+            this.FileView.Name = "FileView";
+            this.FileView.Size = new System.Drawing.Size(302, 315);
+            this.FileView.TabIndex = 1;
+            this.FileView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // CreateFiles
+            // 
+            this.CreateFiles.Location = new System.Drawing.Point(12, 109);
+            this.CreateFiles.Name = "CreateFiles";
+            this.CreateFiles.Size = new System.Drawing.Size(103, 23);
+            this.CreateFiles.TabIndex = 2;
+            this.CreateFiles.Text = "Create file";
+            this.CreateFiles.UseVisualStyleBackColor = true;
+            this.CreateFiles.Click += new System.EventHandler(this.CreateFiles_Click);
+            // 
+            // Filereading
+            // 
+            this.Filereading.Location = new System.Drawing.Point(473, 87);
+            this.Filereading.Name = "Filereading";
+            this.Filereading.Size = new System.Drawing.Size(341, 160);
+            this.Filereading.TabIndex = 4;
+            this.Filereading.Text = "";
+            this.Filereading.TextChanged += new System.EventHandler(this.Filereading_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(220, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tree view of the file system";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(585, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Content of the file";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(594, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Binary Content";
+            // 
+            // BinaryContent
+            // 
+            this.BinaryContent.Location = new System.Drawing.Point(473, 284);
+            this.BinaryContent.Name = "BinaryContent";
+            this.BinaryContent.Size = new System.Drawing.Size(341, 160);
+            this.BinaryContent.TabIndex = 7;
+            this.BinaryContent.Text = "";
+            this.BinaryContent.TextChanged += new System.EventHandler(this.BinaryContent_TextChanged);
+            // 
+            // DeleteFiles
+            // 
+            this.DeleteFiles.Location = new System.Drawing.Point(12, 138);
+            this.DeleteFiles.Name = "DeleteFiles";
+            this.DeleteFiles.Size = new System.Drawing.Size(103, 23);
+            this.DeleteFiles.TabIndex = 9;
+            this.DeleteFiles.Text = "Delete file";
+            this.DeleteFiles.UseVisualStyleBackColor = true;
+            this.DeleteFiles.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(259, 405);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Current folder";
+            // 
+            // CurrentFolder
+            // 
+            this.CurrentFolder.Enabled = false;
+            this.CurrentFolder.Location = new System.Drawing.Point(153, 424);
+            this.CurrentFolder.Name = "CurrentFolder";
+            this.CurrentFolder.Size = new System.Drawing.Size(302, 20);
+            this.CurrentFolder.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 546);
+            this.Controls.Add(this.CurrentFolder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DeleteFiles);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BinaryContent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Filereading);
+            this.Controls.Add(this.CreateFiles);
+            this.Controls.Add(this.FileView);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "File System Simulation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView FileView;
+        private System.Windows.Forms.Button CreateFiles;
+        private System.Windows.Forms.RichTextBox Filereading;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox BinaryContent;
+        private System.Windows.Forms.Button DeleteFiles;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox CurrentFolder;
+    }
+}
+
---
+++ File System Simulation/File System Simulation/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_System_Simulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Fill the tree drives mapping
+            try
+            {
+                FileView.Nodes.Clear();
+                fillTree();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+          
+
+        }
+
+        private void CreateFiles_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string Path = CurrentFolder.Text;
+                string fileName = Path + "\\Persons.spr";
+                FileStream fstPersons = new FileStream(fileName, FileMode.Create);
+                BinaryWriter wrtPersons = new BinaryWriter(fstPersons);
+                wrtPersons.Write("James Bloch");
+                wrtPersons.Write("Catherina Wallace");
+                wrtPersons.Write("Bruce Lamont");
+                wrtPersons.Write("Douglas Truth");
+                wrtPersons.Close();
+                fstPersons.Close();
+              //////////////////////////////////
+              //////////////////////////////////
+              /// Refresh the Treeview://///////
+              /// /////////////////////////////
+              /////////////////////////////////
+                try
+                {
+                    FileView.Nodes.Clear();
+                    fillTree();
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            
+            
+            
+        }
+
+        private void fillTree()
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            foreach (string dr in drives)
+            {
+                TreeNode node = new TreeNode(dr);
+                node.Tag = dr;
+                node.ImageIndex = 0; // drive icon
+                node.Tag = dr;
+                FileView.Nodes.Add(node);
+                node.Nodes.Add(new TreeNode("?"));
+            }
+            FileView.BeforeExpand += new TreeViewCancelEventHandler(treeView1_BeforeExpand);
+        }
+
+        void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if ((e.Node.Nodes.Count == 1) && (e.Node.Nodes[0].Text == "?"))
+            {
+                RecursiveDirWalk(e.Node);
+            }
+        }
+
+        private TreeNode RecursiveDirWalk(TreeNode node)
+        {
+            string path = (string)node.Tag;
+            node.Nodes.Clear();
+           
+            try
+            {
+                string[] dirs = System.IO.Directory.GetDirectories(path);
+                for (int t = 0; t < dirs.Length; t++)
+                {
+                    TreeNode n = new TreeNode(dirs[t].Substring(dirs[t].LastIndexOf('\\') + 1));
+                    n.ImageIndex = 1; // dir icon
+                    n.Tag = dirs[t];
+                    node.Nodes.Add(n);
+                    n.Nodes.Add(new TreeNode("?"));
+                }
+
+                // Optional if you want files as well:
+                string[] files = System.IO.Directory.GetFiles(path);
+                for (int t = 0; t < files.Length; t++)
+                {
+                    TreeNode tn = new TreeNode(System.IO.Path.GetFileName(files[t]));
+                    tn.Tag = files[t];
+                    tn.ImageIndex = 2; // file icon
+                    node.Nodes.Add(tn);
+                } // end of optional file part
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            
+            
+
+            return node;
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+            try
+            {
+                CurrentFolder.Text = FileView.SelectedNode.FullPath;
+                FileStream fleReader = new FileStream(FileView.SelectedNode.FullPath, FileMode.Open, FileAccess.Read);
+                StreamReader stmReader = new StreamReader(fleReader);
+                StringBuilder lstChars = new StringBuilder();
+
+                int iChar = stmReader.Read();
+                lstChars.Append((char)iChar);
+                while (iChar > -1)
+                {
+                    iChar = stmReader.Read();
+                    lstChars.Append((char)iChar);
+                }
+
+                this.Filereading.Text = lstChars.ToString();
+                stmReader.Close();
+                fleReader.Close();
+            }
+            catch (System.Exception ex)
+            {
+               // MessageBox.Show(ex.Message.ToString());
+            }
+
+            try
+            {
+                DisplayBinaryFile();
+            }
+            catch (System.Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+        private void Filereading_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        void DisplayBinaryFile()
+        {
+            int nCols = 16;
+            FileStream inStream = new FileStream(FileView.SelectedNode.FullPath, FileMode.Open,
+                                                             FileAccess.Read);
+            long nBytesToRead = inStream.Length;
+            if (nBytesToRead > 65536 / 4)
+                nBytesToRead = 65536 / 4;
+
+            int nLines = (int)(nBytesToRead / nCols) + 1;
+            string[] lines = new string[nLines];
+            int nBytesRead = 0;
+
+            for (int i = 0; i < nLines; i++)
+            {
+                StringBuilder nextLine = new StringBuilder();
+                nextLine.Capacity = 4 * nCols;
+
+                for (int j = 0; j < nCols; j++)
+                {
+                    int nextByte = inStream.ReadByte();
+                    nBytesRead++;
+                    if (nextByte < 0 || nBytesRead > 65536)
+                        break;
+                    char nextChar = (char)nextByte;
+                    if (nextChar < 16)
+                        nextLine.Append(" x0" + string.Format("{0,1:X}",
+                                                             (int)nextChar));
+                    else if
+                        (char.IsLetterOrDigit(nextChar) ||
+                                              char.IsPunctuation(nextChar))
+                        nextLine.Append("  " + nextChar + " ");
+                    else
+                        nextLine.Append(" x" + string.Format("{0,2:X}",
+                                               (int)nextChar));
+                }
+                lines[i] = nextLine.ToString();
+            }
+            inStream.Close();
+            this.BinaryContent.Lines = lines;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = CurrentFolder.Text;
+            // Delete the file if it exists. 
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            
+             //////////////////////////////////
+              //////////////////////////////////
+              /// Refresh the Treeview://///////
+              /// /////////////////////////////
+              /////////////////////////////////
+                try
+                {
+                    FileView.Nodes.Clear();
+                    fillTree();
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                
+          
+        }
+
+        private void BinaryContent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+             
+
+    
+    }
+}
